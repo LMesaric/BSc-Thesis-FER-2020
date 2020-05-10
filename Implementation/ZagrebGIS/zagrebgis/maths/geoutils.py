5a1eef5f47ffbab2298f3d1e5d4ea6c008f7fd8f
LMesaric <luka.mesaric@fer.hr>
2020-05-10 21:24:40 +0200

    Add util for calculating distance and destination

---
+++ Implementation/ZagrebGIS/zagrebgis/maths/geoutils.py
@@ -0,0 +1,52 @@
+from __future__ import annotations
+from math import sqrt, sin, cos, atan2, asin, radians, degrees
+
+_R = 6371008.8
+"""Earth's mean radius in meters."""
+
+
+class Geolocation:
+    """Geolocation with latitude and longitude in degrees."""
+
+    def __init__(self, lat: float, long: float):
+        self.lat = lat
+        self.long = long
+
+    def __repr__(self):
+        return f"({self.lat}, {self.long})"
+
+    def distance(self, p: Geolocation) -> float:
+        """Calculates distance between `self` and `p` using the haversine formula.
+        https://www.movable-type.co.uk/scripts/latlong.html
+
+        :param p: Other point
+        :return: Distance in meters
+        """
+
+        lat1 = radians(self.lat)
+        long1 = radians(self.long)
+        lat2 = radians(p.lat)
+        long2 = radians(p.long)
+
+        a = (1 - cos(lat1 - lat2)) / 2.0 + \
+            cos(lat1) * cos(lat2) * (1 - cos(long1 - long2)) / 2.0
+        c = 2 * atan2(sqrt(a), sqrt(1 - a))
+        return _R * c
+
+    def destination(self, distance: float, bearing: float) -> Geolocation:
+        """Calculates the destination point with `self` as the starting point.
+
+        :param distance: Distance to travel, in meters
+        :param bearing: Bearing in radians
+        :return: Destination point
+        """
+
+        lat1 = radians(self.lat)
+        long1 = radians(self.long)
+        ang_dist = distance / _R
+
+        lat2 = asin(sin(lat1) * cos(ang_dist) + cos(lat1) * sin(ang_dist) * cos(bearing))
+        long2 = long1 + atan2(sin(bearing) * sin(ang_dist) * cos(lat1),
+                              cos(ang_dist) - sin(lat1) * sin(lat2))
+
+        return Geolocation(degrees(lat2), degrees((long2 + 540) % 360 - 180))
